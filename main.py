04207e46fdafa846f30f5a3b54d262f72212ebc3
卡莫拉 <agh.9876540@gmail.com>
2025-05-25 01:57:49 +0800

    #2 修复数值bug，规范命名

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 #40级yangyang+90级角，仅用于测试
 
 character_atk = 100
+character_level = 40
+
 weapon_atk = 587
 weapon_ratio = 0
-fix_ratio = 0.18
-fix_atk = 505
-skill_ratio = 0.225
-chain_ratio = 0
+
+fix_atk_ratio = 0.18
+fix_atk = 50
+
+dis_enemy_multiplier = 0
+character_dmg_amplify = 0
+element_dmg_increase = 0
+atk_type_dmg_increase = 0
+skill_atk_ratio = 0.225
+chain_atk_ratio = 0
+
 res_ratio = 0
 dis_res_ratio = 0
 enemy_level = 90
-character_level = 40
-dis_enemy_multiplier = 0
 basic_dmg_reduce_ratio = 0
 extra_dmg_reduce_ratio = 0
 basic_element_reduce_ratio = 0.1
-extra_dmg_reduce_ratio = 0
-element_dmg_increase = 0
-atk_type_dmg_increase = 0
 enemy_dmg_amplify = 0
-character_dmg_amplify = 0
 #基础攻击乘区
-def ATK (character_atk,weapon_atk,weapon_ratio,fix_atk,fix_ratio,skill_ratio):
-    return ((character_atk + weapon_atk )* (1 + weapon_ratio + fix_ratio) + fix_atk)*skill_ratio
-atk = ATK(character_atk,weapon_atk,weapon_ratio,fix_atk,fix_ratio,skill_ratio)
+def ATK (character_atk,weapon_atk,weapon_ratio,fix_atk,fix_atk_ratio,skill_atk_ratio):
+    return ((character_atk + weapon_atk )* (1 + weapon_ratio + fix_atk_ratio) + fix_atk)*skill_atk_ratio
+atk = ATK(character_atk,weapon_atk,weapon_ratio,fix_atk,fix_atk_ratio,skill_atk_ratio)
 
 #减免乘区
 def enemyElementRES(res_ratio,dis_res_ratio):
@@ -36,7 +39,7 @@ def enemyElementRES(res_ratio,dis_res_ratio):
         return 1 / (1 + (5 * ratio_sum))
 element_res = enemyElementRES(res_ratio,dis_res_ratio)
 
-def enemyMultiplier(enemy_level,character_level,dis_enemy_multiplier):
+def enemyMultiplier(enemy_level,character_level,dis_enemy_multiplier): #防御乘数百分比
     return (800 + 8 * character_level) / ((800 + 8 * character_level) + ((8 * enemy_level) + 792) * (1 - dis_enemy_multiplier))
 enemy_multiplier = enemyMultiplier(enemy_level,character_level,dis_enemy_multiplier)
 
@@ -44,9 +47,9 @@ def reduceDMG(basic_dmg_reduce_ratio,extra_dmg_reduce_ratio):
     return 1 - (basic_dmg_reduce_ratio + extra_dmg_reduce_ratio)
 reduce_dmg = reduceDMG(basic_dmg_reduce_ratio,extra_dmg_reduce_ratio)
 
-def reduceELEMENT(basic_element_reduce_ratio,extra_dmg_reduce_ratio):
+def reduceElement(basic_element_reduce_ratio,extra_dmg_reduce_ratio):
     return 1 - (basic_element_reduce_ratio + extra_dmg_reduce_ratio)
-reduce_element = reduceELEMENT(basic_element_reduce_ratio,extra_dmg_reduce_ratio)
+reduce_element = reduceElement(basic_element_reduce_ratio,extra_dmg_reduce_ratio)
 
 def reducement(element_res,enemy_multiplier,reduce_dmg,reduce_element):
     return element_res * enemy_multiplier * reduce_dmg * reduce_element
